e259080914b4b8f14f16b5bb2229c5b5c7549f6c
Sandor Kiss <skiss4@gmail.com>
2014-02-18 20:38:05 -0600

    Added multiplication

--- GitTest/UnitTests/com/ic/skiss/MyCalculatorTest.java
+++ GitTest/UnitTests/com/ic/skiss/MyCalculatorTest.java
@@ -6,10 +6,15 @@ import org.junit.Test;
 
 public class MyCalculatorTest {
 
-	MyCalculator myCalculator = new MyCalculator();
-	
-	@Test
-	public void shouldAddRetuns2() {
-		assertEquals("Should equal: ", 2, myCalculator.myAdd(1, 1));
-	}
+    MyCalculator myCalculator = new MyCalculator();
+
+    @Test
+    public void shouldAddReturnTwo() {
+	assertEquals("Should equal: ", 2, myCalculator.myAdd(1, 1));
+    }
+    
+    @Test
+    public void shouldMultiplyReturnTen() {
+	assertEquals("Should equal: ", 10, myCalculator.myMultiply(2, 5));
+    }
 }
--- GitTest/src/com/ic/skiss/MyCalculator.java
+++ GitTest/src/com/ic/skiss/MyCalculator.java
@@ -1,13 +1,18 @@
 package com.ic.skiss;
 
 public class MyCalculator {
-	/**
-	 * Simple class that will be checked into GitHub repository
-	 * @param a integer to be added together
-	 * @param b integer to be added together
-	 * @return the sum of the two input parameters
-	 */
-	public int myAdd(int a, int b) {
-		return a + b;
-	}
+    /**
+     * Simple class that will be checked into GitHub repository
+     * 
+     * @param a integer to be added together
+     * @param b integer to be added together
+     * @return the sum of the two input parameters
+     */
+    public int myAdd(int i, int j) {
+	return i + j;
+    }
+
+    public int myMultiply(int i, int j) {
+	return i * j;
+    }
 }
